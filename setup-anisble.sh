4d0bfad336f300be105a6a42cf3c1167121c5c56
Mentholiptuz <mentholiptuz@gmail.com>
2021-12-16 08:16:34 +0700

    Update setup-anisble.sh

--- setup-anisble.sh
+++ setup-anisble.sh
@@ -1,11 +1,20 @@
+# Setup SSH keys and share it among managed nodes
 ssh-keygen
+
+# To share the ssh keys between control to managed hosts, run ssh-copy-id command
+ssh-copy-id timeserver01.ntpserver.local
+ssh-copy-id timeserver02.ntpserver.local
+ssh-copy-id timeserver03.ntpserver.local
+ssh-copy-id timeserver04.ntpserver.local
+
+# Create ansible cfg and inventory file
 mkdir ntpservers
-cp /etc/ansible/ansible.cfg ~/administrator/
-vi ~/administrator/ansible.cfg
+cp /etc/ansible/ansible.cfg ~/ntpservers/
+vi ~/ntpservers/ansible.cfg
 
 # Add below line under [default]
-inventory      = /home/sysops/administrator/inventory
-remote_user = sysops
+inventory      = /home/administrator/ntpservers/inventory
+remote_user = administrator
 host_key_checking = False
 
 # Add below line under [privilege_escalation]
@@ -14,8 +23,7 @@ become_method=sudo
 become_user=root
 become_ask_pass=False
 
-
-vi ~/administrator/inventory
+vi ~/administrator/ntpservers/inventory
 
 # Add Server List to inventory file
 [ntpservers]
@@ -24,6 +32,7 @@ timeserver02.ntpserver.local
 timeserver03.ntpserver.local
 timeserver04.ntpserver.local
 
-export ANSIBLE_CONFIG=/home/sysops/demo/ansible.cfg
-
+# Declaring ANSIBLE_CONFIG variable
+export ANSIBLE_CONFIG=/home/administrator/ntpservers/ansible.cfg
+echo "export ANSIBLE_CONFIG=/home/administrator/ntpservers/ansible.cfg" >> ~/.profile
 alias chronycstatus="ansible all -m shell -a 'chronyc activity;chronyc sources -v;chronyc tracking'"
